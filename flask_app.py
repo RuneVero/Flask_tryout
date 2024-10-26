2643413a1822eaed17072b605dc18ef40ad3cdbc
Rune <runeverougstraete@gmail.com>
2024-10-26 16:05:43 +0200

    Set flask_app.py as main file

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 def home():
-    return "<p>Hello world!</p>"
+    return "<p>Welkom bij mijn Flask-applicatie!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
